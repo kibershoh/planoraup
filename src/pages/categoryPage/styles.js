5a44808fa9285aca40ef10f5ca2dc4caad5add27
Oybek <saminovo150947@gmail.com>
2025-05-07 00:22:37 +0500

    v

--- src/pages/categoryPage/styles.js
+++ src/pages/categoryPage/styles.js
@@ -26,7 +26,7 @@ export const SubcategoryList = styled.div`
   @media (max-width: 768px) {
       grid-template-columns: repeat(2, 1fr);
       gap:8px;
-      margin-top:5px;
+      margin-top:25px;
 
    
   }
